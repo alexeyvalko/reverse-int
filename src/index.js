7c01795dc57ee28f14690836651b88070926b6a0
alexeyvalko <valko.alexey@gmail.com>
2021-03-14 18:25:01 +0300

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let str = n + "";
+
+    return str.split("").reverse().join("") > 0
+        ? str.split("").reverse().join("")
+        : str.split("").reverse().join("").replace("-", "");
+};
